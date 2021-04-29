a4b1ed70ba66883b97a27810dc9bbf5e18cf9c46
Solidshots <seanbrent5@gmail.com>
2021-04-29 08:45:47 -0400

    refactored animal functions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const {animals} = require('./data/animals.json');
 const express = require('express');
-const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const {filterByQuery, findByID, createNewAnimal, validateAnimal} = require('./lib/animals');
 
 const app = express();
 //parse incoming string or array data
@@ -12,65 +12,6 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3001;
-
-function filterByQuery(query, animalsArray){
-    let filteredResults = animalsArray;
-    let personalityTraitsArray = [];
-
-    if(query.personalityTraits){
-        if(typeof query.personalityTraits === 'string'){
-            personalityTraitsArray = [query.personalityTraits];
-        }
-        else{
-            personalityTraitsArray = query.personalityTraits;
-        }
-
-        personalityTraitsArray.forEach(trait =>{
-            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
-        })
-    }
-
-    if(query.diet){
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-
-    if(query.species){
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-
-    if(query.name){
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-
-    return filteredResults;
-}
-
-function findByID(id, animalsArray){
-    for(var i = 0; i < animalsArray.length; i++){
-        if(animalsArray[i].id == id){
-            return animalsArray[i];
-        }
-    }
-}
-
-function createNewAnimal(body, animalsArray){
-    const animal = body;
-    animalsArray.push(animal);
-
-    fs.writeFileSync(path.join(__dirname, './data/animals.json'), JSON.stringify({animals: animalsArray}, null, 2));
-
-    return animal;
-}
-
-function validateAnimal(animal){
-
-    if(!animal.name || typeof animal.name !== 'string'){return false}
-    if(!animal.species || typeof animal.species !== 'string'){return false}
-    if(!animal.diet || typeof animal.diet !== 'string'){return false}
-    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){return false}
-
-    return true;
-}
 
 app.get('/api/animals', (req, res)=>{
     let results = animals;
