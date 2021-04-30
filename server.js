d2e74ea0883156e564d219365bb7ef6e45283662
Solidshots <seanbrent5@gmail.com>
2021-04-30 13:17:59 -0400

    modularized routes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const {animals} = require('./data/animals.json');
 const express = require('express');
-const path = require('path');
-const {filterByQuery, findByID, createNewAnimal, validateAnimal} = require('./lib/animals');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 //parse incoming string or array data
 app.use(express.urlencoded({extended:true}));
 //parse incoming JSON data
@@ -11,57 +12,10 @@ app.use(express.json());
 //serves static assets from the public directory
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3001;
-
-app.get('/api/animals', (req, res)=>{
-    let results = animals;
-    
-    if(req.query){
-        results = filterByQuery(req.query, results);
-    }
-    
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) =>{
-    let result = findByID(req.params.id, animals);
-
-    if(result){
-        res.json(result);
-    }
-    else{
-        res.send(404);
-    }
-    
-});
-
-app.post('/api/animals', (req, res) =>{
-    req.body.id = animals.length.toString();
-
-    if(!validateAnimal(req.body)){
-        res.status(400).send("Animal submission was not properly formatted.");
-    }
-    else{
-        const animal = createNewAnimal(req.body, animals);
-        res.json(req.body);
-    }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get('/animals', (req, res) =>{
-    res.sendFile(path.join(__dirname, "./public/animals.html"));
-});
-
-app.get('/zookeepers', (req, res) =>{
-    res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
-});
-
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+//tells our server to use api routes if /api is included in the route
+app.use('/api', apiRoutes);
+//tells our server to serve html routes if only / is included in the route
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () =>{
     console.log(`API server now on port ${PORT}!`);
